871b53b79fa3efe849cc5b7e546b982c075023b3
unknown <akilamaduranga91@gmail.com>
2026-02-04 15:24:27 +0530

    Query search input fields

--- debug_schema.sh
+++ debug_schema.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# Script to get fields of Torrent type (for file size)
+# Script to discover contentType filter options in search input
 
-echo "Getting Torrent fields..."
+echo "Checking TorrentContentSearchQueryInput fields..."
 echo ""
 
 curl -s -X POST http://localhost:3333/graphql \
   -H "Content-Type: application/json" \
-  -d '{"query":"{ __type(name: \"Torrent\") { fields { name type { name kind ofType { name } } } } }"}' | python3 -m json.tool
+  -d '{"query":"{ __type(name: \"TorrentContentSearchQueryInput\") { inputFields { name type { name kind ofType { name } } } } }"}' | python3 -m json.tool
 
 echo -e "\n\nDone."
