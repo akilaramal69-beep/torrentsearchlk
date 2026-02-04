7c7c05faf1449c6ad29b45207fc3bc8e9bf6592b
unknown <akilamaduranga91@gmail.com>
2026-02-04 14:55:38 +0530

    Query Torrent type for size

--- debug_schema.sh
+++ debug_schema.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# Script to get fields of TorrentContent type
+# Script to get fields of Torrent type (for file size)
 
-echo "Getting TorrentContent fields..."
+echo "Getting Torrent fields..."
 echo ""
 
 curl -s -X POST http://localhost:3333/graphql \
   -H "Content-Type: application/json" \
-  -d '{"query":"{ __type(name: \"TorrentContent\") { fields { name type { name kind ofType { name } } } } }"}' | python3 -m json.tool
+  -d '{"query":"{ __type(name: \"Torrent\") { fields { name type { name kind ofType { name } } } } }"}' | python3 -m json.tool
 
 echo -e "\n\nDone."
