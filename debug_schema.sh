d745bcdc87c482357767c449e0f39972babc60c8
unknown <akilamaduranga91@gmail.com>
2026-02-04 14:47:39 +0530

    Improve schema discovery

--- debug_schema.sh
+++ debug_schema.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
-# Script to discover available fields in Bitmagnet GraphQL schema
+# Script to discover available types in Bitmagnet GraphQL schema
 
-echo "Testing available fields..."
+echo "Querying schema types..."
 echo ""
 
-# Test with introspection query to get the schema
+# Get all types in the schema
 curl -s -X POST http://localhost:3333/graphql \
   -H "Content-Type: application/json" \
-  -d '{"query":"{ __type(name: \"TorrentContentSearchResultItem\") { fields { name type { name kind ofType { name } } } } }"}' | python3 -m json.tool
+  -d '{"query":"{ __schema { types { name kind } } }"}' | python3 -m json.tool | grep -i torrent
+
+echo -e "\n\n--- Now trying to get fields from a search result ---"
+
+# Try to get a real result and see what fields come back by default
+curl -s -X POST http://localhost:3333/graphql \
+  -H "Content-Type: application/json" \
+  -d '{"query":"{ torrentContent { search(input: { queryString: \"test\" }) { items { __typename } } } }"}' | python3 -m json.tool
 
 echo -e "\n\nDone."
