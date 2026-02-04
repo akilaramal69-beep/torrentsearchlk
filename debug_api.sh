17e4b6014696ad359884f6e521facfeb96dd2d4c
unknown <akilamaduranga91@gmail.com>
2026-02-04 14:09:59 +0530

    Add debug API script

---
+++ debug_api.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Script to debug GraphQL API
+
+echo "Testing Bitmagnet API..."
+
+# Note: Using localhost:3333 directly since we are on the server (hopefully)
+# If this is run on the host machine, it should reach the docker mapped port
+curl -v -X POST http://localhost:3333/graphql \
+  -H "Content-Type: application/json" \
+  -d '{"query":"query Search($query: String!) { torrentContent { search(query: $query) { items { infoHash title } } } }","variables":{"query":"test"}}'
+
+echo -e "\n\nDone."
